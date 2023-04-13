d63a4d9a46027a17de23eb8f57fd0fd35e1c27c0
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 14:42:47 +0530

    Create B 205 : C D01 - Prime Testing - 1.c

---
+++ B 205 : C D01 - Prime Testing - 1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("no");
+    }
+    else if (n==2 || n==3)
+    {
+        printf("yes");
+    }
+    else if (n%2==0 || n%3==0)
+    {
+        printf("no");
+    }
+    else
+    {
+        for(i=5;i*i<=n;i+=6)
+        {
+            if (n%i==0 || n%(i+2)==0)
+            {
+                 printf("no");
+                 break;
+            }
+        }
+        printf("yes");
+    }
+}
