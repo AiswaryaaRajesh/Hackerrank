e16bb5d94c68b1dec00b4b571fc08d55899f886d
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 14:28:35 +0530

    Create B 103 : Check if a Given Number is Even or Odd.c

---
+++ B 103 : Check if a Given Number is Even or Odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1;
+	scanf("%d", &var1);
+	if (_)//Replace the '_' with appropriate conditions and statements
+	{
+		printf(_);
+	}
+	else
+	{
+		printf(_);
+	}
+	return 0;
+}
