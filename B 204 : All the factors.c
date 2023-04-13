ab8e7d7a5b35055e8980969a750d1fed73289a0c
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 14:42:13 +0530

    Create B 204 : All the factors.c

---
+++ B 204 : All the factors.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+        int num, i;
+        scanf("%d", &num);
+        for (i = 1; i <= num; ++i) 
+            if (num % i == 0) 
+                printf("%d ", i);
+    return 0;
+}
