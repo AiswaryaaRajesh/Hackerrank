e752785a2e8b469bb8d9886f2e46bc5757254a02
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 16:00:04 +0530

    Create AMAZON_25 Implement pow(x,n).c

---
+++ AMAZON_25 Implement pow(x,n).c
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    long double x,y;
+    cin>>x>>y;
+    long double res=pow(x,y);
+    cout<<res;
+    return 0;
+}
