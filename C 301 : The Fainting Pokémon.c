391ab6f501f0a5fbcd476611771f65df97c64fe6
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 14:53:42 +0530

    Create C 301 : The Fainting Pokémon.c

---
+++ C 301 : The Fainting Pokémon.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N,sp,i;
+    scanf("%d%d",&sp,&N);
+    int a[N];
+   
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+        if(sp<=a[i])
+        {
+            printf("-1");
+            return 0;
+        }
+    }
+        int day=1;
+        int x=sp;
+   
+        for(i=0;i<N;)
+        {
+            if(a[i]<x)
+            {
+                x=x-a[i];
+                i++;
+            }
+            else
+            {
+                day++;
+                x=sp;
+            }
+               
+        }
+    printf("%d",day);  
+   
+   
+    return 0;
+}
