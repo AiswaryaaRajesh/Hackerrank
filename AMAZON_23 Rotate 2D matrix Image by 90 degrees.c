d46fd9ba490a8b9fec7441aaeb3e719ac6fe64a6
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 15:34:01 +0530

    Create AMAZON_23 Rotate 2D matrix Image by 90 degrees.c

---
+++ AMAZON_23 Rotate 2D matrix Image by 90 degrees.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void rotateImage(int matrix[][50], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int temp = matrix[i][start];
+            matrix[i][start] = matrix[i][end];
+            matrix[i][end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[50][50];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    rotateImage(matrix, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
