ddf9083d0c0fa0eeea6416ae0033c59f4d993985
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2023-04-13 14:43:20 +0530

    Create B 206 : The World of Numbers.c

---
+++ B 206 : The World of Numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n1,n2;
+    scanf("%d %d",&n1,&n2);
+    printf("%.0f",pow(n1,n2));
+    return 0;
+}
